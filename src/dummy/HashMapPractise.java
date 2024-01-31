f3aca6428495e7484ecb7252ea5479cd126c5345
aomkar2421 <omkarjagtap1651@gmail.com>
2024-01-31 22:45:50 +0530

    31-January

--- src/dummy/HashMapPractise.java
+++ src/dummy/HashMapPractise.java
@@ -1,20 +1,15 @@
-package array;
+package dummy;
 
 import java.util.HashMap;
 
-public class RemoveDuplicates3 {
+public class HashMapPractise {
 	public static void main(String[] args) {
 		
+		HashMap<Integer, index> hs = new HashMap();
 		int [] arr = {2 ,3 ,3 ,9 ,9 ,11 ,23 , 57 ,57 ,99 ,101 ,101 ,102};
-		
-		HashMap<Integer, Integer> hs = new HashMap<>();
-		
 		for (int i = 0; i < arr.length; i++) {
-			hs.put(i, arr[i]);
-		}
-		
-		System.out.println(hs.get(8));
 			
+		}
 		
 	}
 }
