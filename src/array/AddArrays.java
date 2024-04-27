3daba4a9b9ed927fd88f2294159c83094c9cdd5b
aomkar2421 <omkarjagtap1651@gmail.com>
2024-04-27 17:34:32 +0530

    27 April

--- src/array/AddArrays.java
+++ src/array/AddArrays.java
@@ -1,26 +1,30 @@
 package array;
-	
+
 public class AddArrays {
 	public static void main(String[] args) {
-		int [] arr1 = {12,34,56,76};
-		int [] arr2 = {45,22,62,90,12,34,50};
-		int max = 0;
-		
-		if (arr1.length > arr2.length) {
-			max = arr1.length;
-		} else {
-			max = arr2.length;
-		}
-		int [] sum = new int[max];
-		
-		for (int i = 0; i < max; i++) {
-			int a = (arr1.length > i) ? arr1[i] : 0 ;
-			int b = (arr2.length > i) ? arr2[i] : 0 ;
-			sum[i] = a + b;
-		}
-		
-		Printa.print(sum);
-		
+		int [] arr2 = {12,34,56,76};
+		int [] arr1 = {45,22,62,90,12,34,50};
+
+		addArrays(arr1,arr2);
 	}
+
+	private static void addArrays(int[] arr1, int[] arr2) {
+		int max = Integer.max(arr1.length,arr2.length);
+
+		int [] sum = new int [max];
+
+		if(arr1.length > arr2.length) {
+			for (int i = 0; i < max; i++) {
+				sum[i] = arr1[i] + (i > arr2.length-1 ? 0 : arr2[i]);
+			}
+		}else {
+			for (int i = 0; i < max; i++) {
+				sum[i] = arr2[i] +( i > arr1.length-1 ? 0 : arr1[i]);
+			}
+		}
+		Printa.print(sum);
+	}
+
+
+
 }	
-	
---
+++ src/array/TwoSum.java
@@ -0,0 +1,9 @@
+package array;
+
+public class TwoSum {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
